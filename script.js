c97c3f3f2d55b84dd611434db86b3bba31c82d09
Balia077 <balaramd894@gmail.com>
2025-06-06 09:46:10 +0530

    add swiper functionality and styles for pages 4 and 5, including footer adjustments

--- script.js
+++ script.js
@@ -21,3 +21,13 @@ elems.forEach(function(e){
         fixed.style.backgroundImage = `url(${image})`
     })
 })
+
+  var swiper = new Swiper(".mySwiper", {
+      slidesPerView: "auto",
+      centeredSlides: true,
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
